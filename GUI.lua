2b30c1ffedd83534bf4d067efc9fb6a018beec3a
Choumiko <ghostface@ghostface.info>
2015-02-04 09:38:40 +0100

    localisation for gui

--- GUI.lua
+++ GUI.lua
@@ -35,15 +35,9 @@ GUI = {
     local buttons = GUI.add(rows, {type="table", name="buttons", colspan=3})
     GUI.add(buttons, {type="button", name="start"})
     GUI.add(buttons, {type="button", name="cc"})
-    GUI.add(buttons, {type="button", name="settings", caption="S"})
-    --local stgsSignals = GUI.add(rows,{type="table", name="stgsSignals", colspan=2})
-    GUI.add(rows, {type="checkbox", name="signals", caption="Place signals"}, "signals")
-    --GUI.add(stgsSignals,{type="checkbox", name="flipSignals", caption="reverse"}, "flipSignals")
-    GUI.add(rows, {type="checkbox", name="poles", caption="Place poles"}, "poles")
-    --local buttons2 = GUI.add(rows,{type="table", name="junctions", colspan=3})
-    --GUI.add(buttons2, {type="label", caption="Junction"})
-    --GUI.add(buttons2, {type="button", name="junctionLeft", caption="left"})
-    --GUI.add(buttons2, {type="button", name="junctionRight", caption="right"})
+    GUI.add(buttons, {type="button", name="settings", caption={"text-settings"}})
+    GUI.add(rows, {type="checkbox", name="signals", caption={"tgl-signal"}}, "signals")
+    GUI.add(rows, {type="checkbox", name="poles", caption={"tgl-poles"}}, "poles")
   end,
 
   destroyGui = function(player)
@@ -63,13 +57,13 @@ GUI = {
     elseif name == "settings" then
       GUI.toggleSettingsWindow(player)
     elseif name == "side" then
-      if event.element.caption == "right" then
+      if glob.settings.poleSide == 1 then
         glob.settings.poleSide = -1
-        event.element.caption = "left"
+        event.element.caption = {"stg-side-left"}
         return
       else
         glob.settings.poleSide = 1
-        event.element.caption = "right"
+        event.element.caption = {"stg-side-right"}
         return
       end
     elseif name == "cc" then
@@ -87,9 +81,9 @@ GUI = {
     local row = player.gui.left.farl.rows
     local captionSide
     if glob.settings.poleSide == 1 then
-      captionSide = "right"
+      captionSide = {"stg-side-right"}
     else
-      captionSide = "left"
+      captionSide = {"stg-side-left"}
     end
     if row.settings ~= nil then
       local s = row.settings
@@ -100,23 +94,21 @@ GUI = {
       sDistance = sDistance < 0 and 0 or sDistance
       local weight = tonumber(s.curvedWeight.text) or glob.settings.curvedWeight
       weight = weight < 0 and 1 or weight
-      player.gui.left.farl.rows.buttons.settings.caption="S"
+      player.gui.left.farl.rows.buttons.settings.caption={"text-settings"}
       GUI.saveSettings{poleDistance=pDistance, signalDistance=sDistance, curvedWeight=weight}
       row.settings.destroy()
     else
       local settings = row.add({type="table", name="settings", colspan=2})
-      player.gui.left.farl.rows.buttons.settings.caption="Save settings"
-      settings.add({type="label", caption="Distance between pole and rail", style="farl_label"})
+      player.gui.left.farl.rows.buttons.settings.caption={"text-save"}
+      settings.add({type="label", caption={"stg-poleDistance"}, style="farl_label"})
       local pDistance = settings.add({type="textfield", name="poleDistance", style="farl_textfield_small"})
+      GUI.add(settings, {type="checkbox", name="medium", caption={"stg-mediumPoles"}},"medium")
       local row1 = GUI.add(settings,{type="table", name="row2", colspan=2})
-      GUI.add(row1, {type="label", caption="Side of pole:", style="farl_label"})
+      GUI.add(row1, {type="label", caption={"stg-poleSide"}, style="farl_label"})
       GUI.add(row1, {type="button", name="side", caption=captionSide, style="farl_button"})
-      GUI.add(settings, {type="checkbox", name="medium", caption="use medium Poles"},"medium")
-      settings.add({type="label", caption="Side of pole:", style="farl_label"})
-      settings.add({type="button", name="side", caption=captionSide, style="farl_button"})
-      settings.add({type="label", caption="Distance between rail signals", style="farl_label"})
+      settings.add({type="label", caption={"stg-signalDistance"}, style="farl_label"})
       local sDistance = settings.add({type="textfield", name="signalDistance", style="farl_textfield_small"})
-      settings.add({type="label", caption="Weight for curved rails", style="farl_label"})
+      settings.add({type="label", caption={"stg-curvedWeight"}, style="farl_label"})
       local weight = settings.add({type="textfield", name="curvedWeight", style="farl_textfield_small"})
       pDistance.text = glob.settings.poleDistance
       sDistance.text = glob.settings.signalDistance
@@ -134,8 +126,8 @@ GUI = {
 
   updateGui = function(farl)
     if farl.driver.name ~= "farl_player" then
-      farl.driver.gui.left.farl.rows.buttons.start.caption = farl.active and "Stop" or "Start"
-      farl.driver.gui.left.farl.rows.buttons.cc.caption = farl.cruise and "Stop cruise" or "Cruise"
+      farl.driver.gui.left.farl.rows.buttons.start.caption = farl.active and {"text-stop"} or {"text-start"}
+      farl.driver.gui.left.farl.rows.buttons.cc.caption = farl.cruise and {"text-stopCC"} or {"text-startCC"}
     end
   end,
 }
