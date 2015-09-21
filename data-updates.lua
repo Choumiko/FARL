6680da633bd10b34ddeb3ecfa1eeccef6b21df77
Choumiko <ghostface@ghostface.info>
2015-09-21 09:52:02 +0200

    fix broken graphics with 5dims trains

--- data-updates.lua
+++ data-updates.lua
@@ -2,4 +2,195 @@ table.insert(data.raw["technology"]["rail-signals"].effects,
   {
     type="unlock-recipe",
     recipe = "farl"
-  })  
+  })
+
+
+--fixes for 5dims
+if data.raw["rail"]["straight-power-rail"] then
+  data.raw["locomotive"]["farl"] = {
+    type = "locomotive",
+    name = "farl",
+    icon = "__base__/graphics/icons/diesel-locomotive.png",
+    flags = {"placeable-neutral", "player-creation", "placeable-off-grid", "not-on-map"},
+    minable = {mining_time = 1, result = "farl"},
+    mined_sound = {filename = "__core__/sound/deconstruct-medium.ogg"},
+    max_health = 1000,
+    corpse = "medium-remnants",
+    dying_explosion = "medium-explosion",
+    collision_box = {{-0.6, -2.6}, {0.6, 2.6}},
+    selection_box = {{-0.85, -2.6}, {0.9, 2.5}},
+    drawing_box = {{-1, -4}, {1, 3}},
+    weight = 2000,
+    max_speed = 0.8,
+    max_power = "600kW",
+    braking_force = 10,
+    friction_force = 0.0015,
+    -- this is a percentage of current speed that will be subtracted
+    air_resistance = 0.002,
+    connection_distance = 3.3,
+    joint_distance = 4.6,
+    energy_per_hit_point = 5,
+    resistances =
+    {
+      {
+        type = "fire",
+        decrease = 15,
+        percent = 50
+      },
+      {
+        type = "physical",
+        decrease = 15,
+        percent = 30
+      },
+      {
+        type = "impact",
+        decrease = 50,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 15,
+        percent = 30
+      },
+      {
+        type = "acid",
+        decrease = 10,
+        percent = 20
+      }
+    },
+    energy_source =
+    {
+      type = "burner",
+      effectivity = 1,
+      fuel_inventory_size = 3,
+      smoke =
+      {
+        {
+          name = "smoke",
+          deviation = {0.1, 0.1},
+          frequency = 210,
+          position = {0, 0},
+          slow_down_factor = 15,
+          starting_frame = 1,
+          starting_frame_deviation = 5,
+          starting_frame_speed = 0,
+          starting_frame_speed_deviation = 1,
+          height = 2,
+          height_deviation = 0.2,
+          starting_vertical_speed = 0.2,
+          starting_vertical_speed_deviation = 0.06,
+        }
+      }
+    },
+    front_light =
+    {
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {-0.6, -16},
+        size = 2,
+        intensity = 0.6
+      },
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {0.6, -16},
+        size = 2,
+        intensity = 0.6
+      }
+    },
+    back_light = rolling_stock_back_light(),
+    stand_by_light = rolling_stock_stand_by_light(),
+    pictures =
+    {
+      priority = "very-low",
+      width = 346,
+      height = 248,
+      direction_count = 256,
+      filenames =
+      {
+        "__FARL__/graphics/entity/farl/farl-01.png",
+        "__FARL__/graphics/entity/farl/farl-02.png",
+        "__FARL__/graphics/entity/farl/farl-03.png",
+        "__FARL__/graphics/entity/farl/farl-04.png",
+        "__FARL__/graphics/entity/farl/farl-05.png",
+        "__FARL__/graphics/entity/farl/farl-06.png",
+        "__FARL__/graphics/entity/farl/farl-07.png",
+        "__FARL__/graphics/entity/farl/farl-08.png"
+      },
+      line_length = 4,
+      lines_per_file = 8,
+      shift = {0.9, -0.45}
+    },
+    rail_category = "regular",
+
+    stop_trigger =
+    {
+      -- left side
+      {
+        type = "create-smoke",
+        repeat_count = 125,
+        entity_name = "smoke-train-stop",
+        initial_height = 0,
+        -- smoke goes to the left
+        speed = {-0.03, 0},
+        speed_multiplier = 0.75,
+        speed_multiplier_deviation = 1.1,
+        offset_deviation = {{-0.75, -2.7}, {-0.3, 2.7}}
+      },
+      -- right side
+      {
+        type = "create-smoke",
+        repeat_count = 125,
+        entity_name = "smoke-train-stop",
+        initial_height = 0,
+        -- smoke goes to the right
+        speed = {0.03, 0},
+        speed_multiplier = 0.75,
+        speed_multiplier_deviation = 1.1,
+        offset_deviation = {{0.3, -2.7}, {0.75, 2.7}}
+      },
+      {
+        type = "play-sound",
+        sound =
+        {
+          {
+            filename = "__base__/sound/train-breaks.ogg",
+            volume = 0.6
+          },
+        }
+      },
+    },
+    drive_over_tie_trigger = drive_over_tie(),
+    tie_distance = 50,
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    working_sound =
+    {
+      sound =
+      {
+        filename = "__base__/sound/train-engine.ogg",
+        volume = 0.4
+      },
+      match_speed_to_activity = true,
+    },
+    open_sound = { filename = "__base__/sound/car-door-open.ogg", volume=0.7 },
+    close_sound = { filename = "__base__/sound/car-door-close.ogg", volume = 0.7 },
+    sound_minimum_speed = 0.5;
+  }
+end
