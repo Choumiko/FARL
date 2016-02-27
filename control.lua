43735e2afcde6c2fcaa5f095d5a579780344b9ab
Choumiko <ghostface@ghostface.info>
2016-02-27 19:56:54 +0100

    bug hunting release

--- control.lua
+++ control.lua
@@ -150,7 +150,8 @@ local function on_configuration_changed(data)
     else
       debugDump("FARL version: "..newVersion,true)
     end
-    if oldVersion and oldVersion < "0.5.11" then
+    if oldVersion and oldVersion < "0.5.12" then
+      debugDump("Reset settings",true)
       global = nil
     end
     on_init()
